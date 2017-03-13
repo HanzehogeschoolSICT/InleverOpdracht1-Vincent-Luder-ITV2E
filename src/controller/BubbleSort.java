181dff3436038107b2891e411fdc0164446323e6
Vincent Luder <vincentluder@hotmail.com>
2017-03-13 15:23:52 +0100

    ArrayList met arrays van de stappen wordt gegenereerd

--- src/controller/BubbleSort.java
+++ src/controller/BubbleSort.java
@@ -9,14 +9,21 @@ import java.util.Random;
  * Created by Gebruiker on 8-3-2017.
  */
 public class BubbleSort {
-
+    ArrayList<Compare[]> arrayList = new ArrayList<>();
     //ArrayList<Compare> compareArrayList = new ArrayList<>();
     //Compare[] compareArray = new Compare[10];
 
-
+    public void printArrayList(){
+        for(int i = 0; i < arrayList.size();i++) {
+            for (int j = 0; j < 10; j++) {
+                Compare[] array = new Compare[10];
+                array = arrayList.get(i);
+                System.out.println(array[j].getValue());
+            }
+        }
+    }
 
     public void sortArray(Compare [] compareArray){
-
 
         boolean nextPass = true;
 
@@ -24,12 +31,14 @@ public class BubbleSort {
             nextPass = false;
 
             for (int j = 0; j < compareArray.length -k; j++){
+
                 if(compareArray[j].compareTo(compareArray[j+1]) == -1){
                     Compare temp = compareArray[j];
                     compareArray[j] = compareArray[j +1];
                     compareArray[j+1] = temp;
-
                     nextPass = true;
+
+
                 }
             }
         }
--- src/view/TestMain.java
+++ src/view/TestMain.java
@@ -4,10 +4,56 @@ import controller.BubbleSort;
 import model.ArrayFiller;
 import model.Compare;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * Created by Gebruiker on 9-3-2017.
  */
 public class TestMain {
+    ArrayList<ArrayList<Integer>> arrayList = new ArrayList<>();
+
+    public void printArrayList(){
+        for(int i = 0; i < arrayList.size();i++) {
+            for (int j = 0; j < 5; j++) {
+                ArrayList<Integer> array = new ArrayList<>();
+                array = arrayList.get(i);
+                System.out.println(array.get(j));
+            }
+        }
+    }
+
+    public  void bubbleSort(int[] numArray) {
+        ArrayList<Integer> tempArray = new ArrayList<>();
+        int n = numArray.length;
+        int temp = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+
+                if (numArray[j - 1] > numArray[j]) {
+                    temp = numArray[j - 1];
+                    numArray[j - 1] = numArray[j];
+                    numArray[j] = temp;
+
+                }
+
+            }
+            for(int inte: numArray){
+                //System.out.println(inte);
+                tempArray.add(inte);
+
+            }
+
+            for(int h:tempArray){
+                System.out.println(h);
+            }
+            arrayList.add(tempArray);
+            tempArray.clear();
+        }
+    }
+
+
 
     public static void main(String[] args) {
         ArrayFiller filler = new ArrayFiller();
@@ -15,10 +61,24 @@ public class TestMain {
         Compare[] array = new Compare[10];
 
         filler.fillArray(10,array);
-        sorter.sortArray(array);
+        //sorter.sortArray(array);
 
-        for(Compare value: array){
-            System.out.println(value.getValue());
-        }
+        //sorter.printArrayList();
+        int[] array1 = new int[5];
+        array1[0] = 10;
+        array1[1] = 5;
+        array1[2] = 7;
+        array1[3] = 4;
+        array1[4] = 1;
+
+        TestMain henk = new TestMain();
+        henk.bubbleSort(array1);
+        //for(int i =0; i< array1.length;i++){
+          //  System.out.println(array1[i]);
+        //}
+
+        henk.printArrayList();
+
+
     }
 }
