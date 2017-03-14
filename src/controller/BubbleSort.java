807ecb5ba2664aa971dba26329ca05e87c8eb78e
redvox27 <v.t.luder@st.hanze.nl>
2017-03-14 15:59:34 +0100

    niek en vincent hebben insertion sort toegevoegd. Referenties. het boek blz 885 en http://www.javatpoint.com/insertion-sort-in-java. Dit is gedaan via skype

--- src/controller/BubbleSort.java
+++ src/controller/BubbleSort.java
@@ -10,27 +10,6 @@ import java.util.Arrays;
  * Created by Gebruiker on 8-3-2017.
  */
 public class BubbleSort {
-    //ArrayList<ArrayList<Compare>> arrayList = new ArrayList<>();
-    //ArrayList<Compare> compareArrayList = new ArrayList<>();
-    //Compare[] compareArray = new Compare[10];
-    //ArrayList<Compare[]> stepArray = new ArrayList<>();
-    boolean pass = true;
-
-    public void printArrayList(ArrayList<Compare[]> arrayList){
-        for(int i = 0; i< arrayList.size();){
-            Compare[] temp = new Compare[10];
-            temp = arrayList.get(i);
-            for(Compare comp: temp){
-                System.out.println(comp.getValue());
-            }
-            arrayList.remove(0);
-            if(arrayList.size() == 0){
-                break;
-            }
-        }
-    }
-
-
 
     public void sortArray(Compare [] compareArray){
 
@@ -45,36 +24,7 @@ public class BubbleSort {
                     for(Compare compare : compareArray){
                         System.out.println(compare.getValue());
                     }
-
                 }
-
             }
-
-        }
-
-
-
-
-
-    public void convertToArray(ArrayList<Compare> arrayList,Compare[] array){
-        for(int i = 0; i < arrayList.size(); i++){
-            array[i] = arrayList.get(i);
         }
     }
-
-    public static void main(String[] args) {
-        ArrayList<Compare[]> stepArray = new ArrayList<>();
-        BubbleSort sorter = new BubbleSort();
-        ArrayFiller filler = new ArrayFiller();
-        Compare[] compareArray = new Compare[10];
-        filler.fillArray(10,compareArray);
-        filler.fillArray(10,compareArray);
-        filler.fillArray(10,compareArray);
-        filler.fillArray(10,compareArray);
-        sorter.sortArray(compareArray);
-
-    }
-
-
-
-}
---
+++ src/controller/InsertionSort.java
@@ -0,0 +1,48 @@
+package controller;
+
+/**
+ * Created by Gebruiker on 14-3-2017.
+ */
+public class InsertionSort {
+    //int[] array = new int[5];
+    int[] temp = new int[5];
+    public void fillArray(int[] array){
+
+        array[0] = 2;
+        array[1] = 9;
+        array[2] = 5;
+        array[3] = 4;
+        array[4] = 8;
+    }
+
+    public void insertionSort(int[] array){
+
+        for (int i = 1; i < array.length; i++) {
+            int sortValue = array[i];
+            int j = i;
+            if (j > 0 && array[j - 1] > sortValue) {
+                array[j] = array[j - 1];
+                j--;
+            }
+            array[j] = sortValue;
+        }
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        InsertionSort sorter = new InsertionSort();
+        int[] array = new int[5];
+
+        sorter.fillArray(array);
+
+        sorter.insertionSort(array);
+       // sorter.insertionSort(array);
+        for(int i : array){
+            System.out.println(i);
+        }
+    }
+
+}
